6a44101ae7358777aa76316299a4299ba7285354
vitegourmand2026 <tombruston@gmail.com>
2026-05-25 02:07:10 +0200

    feat: setup Angular 21, Tailwind, CVA, NgRx Signals, routing, not-found

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    // Angular 21 — pas de Zone.js, changements détectés via Signals
+    provideZonelessChangeDetection(),
+
+    // Active le router + lecture des params :id via @Input()
+    provideRouter(routes, withComponentInputBinding()),
+
+    // Active HttpClient avec l'API fetch native du navigateur
+    provideHttpClient(withFetch()),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/components/not-found/not-found')
+        .then(m => m.NotFound)
+  }
+];
